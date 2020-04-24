c94dbbd97d27056e59d3c6aa087c7bb2fd3991a6
udaypydi <udaypydi333@gmail.com>
2020-04-24 20:03:37 +0530

    feat(eslint): eslint configs added

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: [
+      'plugin:react/recommended',
+      'airbnb',
+    ],
+    settings: {
+      "import/resolver": {
+        node: {
+            extensions: ['.js', '.jsx', '.scss', '.css'],
+            moduleDirectory: ['node_modules', 'src/'],
+        },
+      },
+    },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+    ],
+    rules: {
+    },
+  };

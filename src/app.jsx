8a5dc7a7365fb54243e0be89bc733f59b55b3d11
udaypydi <udaypydi333@gmail.com>
2020-04-24 20:06:52 +0530

    Home page changed

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,8 @@ import './app.scss';
 export default function App() {
   return (
     <div className="bg-blue-900 flex flex-col justify-center items-center p-20">
-      <p className="text-4xl text-blue-100">Welcom To React Boilerplate!</p>
+      <p className="text-4xl text-blue-100">Welcom To Tail UI!</p>
+      <p className="text-2xl text-blue-100">React UI Components Built Using TailwindCSS!</p>
       <img src={logo} className="app--image" alt="logo" />
       <Button buttonText="Get Started!" />
     </div>

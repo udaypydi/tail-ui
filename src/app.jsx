73b56b84527a34e7b3596402f7cf6ec6f8442093
udaypydi <udaypydi333@gmail.com>
2020-04-24 20:17:26 +0530

    feat(homepage): homepage modified

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'uielements/button/button.component';
 import logo from './logo.png';
+import tail from './tail.png';
 import 'assets/css/main.css';
 import './app.scss';
 
@@ -9,7 +10,11 @@ export default function App() {
     <div className="bg-blue-900 flex flex-col justify-center items-center p-20">
       <p className="text-4xl text-blue-100">Welcom To Tail UI!</p>
       <p className="text-2xl text-blue-100">React UI Components Built Using TailwindCSS!</p>
-      <img src={logo} className="app--image" alt="logo" />
+      <div className="flex items-center justify-center"> 
+        <img src={logo} className="app--image" alt="logo" />
+        <p className="text-6xl text-blue-100 img-divider font-bold">+</p>
+        <img src={tail} className="app--image" alt="tail" />
+      </div>
       <Button buttonText="Get Started!" />
     </div>
   );

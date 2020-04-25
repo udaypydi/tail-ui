e838a81d209538444a279aa516d005b7cafbfd54
udaypydi <udaypydi333@gmail.com>
2020-04-25 19:48:01 +0530

    fix(docs): text change

--- src/docs/home/index.jsx
+++ src/docs/home/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="bg-blue-900 flex flex-col justify-center items-center p-20">
-      <p className="text-4xl text-indigo-100">Welcome to docs!</p>
+      <p className="text-4xl text-indigo-100">Welcome to Tail UI Docs!</p>
     </div>
   );
 }

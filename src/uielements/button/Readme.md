d8bcb471d56cb939574c27dca9e5d63c09ea51a1
udaypydi <udaypydi333@gmail.com>
2020-04-26 16:05:42 +0530

    feat(Button): add button docs

--- src/uielements/button/Readme.md
+++ src/uielements/button/Readme.md
@@ -1,5 +1,23 @@
-Button example:
+Simple Button example:
 
 ```js
 <Button />
+```
+
+Button with Text Color:
+
+```js
+<Button color="red" />
+```
+
+Button with Custom Styles:
+
+```js
+<Button style={{ background: '#0077b5', color: '#ffffff' }} />
+```
+
+Button with Tailwind Classes:
+
+```js
+<Button className="text-green-500 border-green-500" />
 ```
--- src/uielements/button/button.component.js
+++ src/uielements/button/button.component.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import 'src/assets/css/main.css';
 /**
  * Captures user click actions.
  */
 
 function Button(props) {
-  const { buttonText, primary, onClick } = props;
+  const { buttonText, primary, onClick, className, style, bgColor, color } = props;
   let btnClass = '';
 
   if (primary) {
-    btnClass = 'bg-transparent hover:bg-blue-500 text-indigo-100 font-semibold hover:text-white py-2 px-4 border border-indigo-100 hover:border-transparent rounded';
+    btnClass = `bg-${bgColor}-700 hover:bg-${bgColor}-500 text-${color}-700 font-semibold hover:text-${color}-700 py-2 px-4 border border-black-700 rounded`;
   }
   return (
     <button
       type="button"
-      className={`${btnClass}`}
+      className={`${btnClass} ${className}`}
       onClick={onClick}
+      style={style}
     >
       {buttonText}
     </button>
@@ -29,19 +30,39 @@ Button.propTypes = {
    */
   buttonText: PropTypes.string,
   /**
-   * renders a button based on the primary theme
+   * Renders a button based on the primary theme
    */
   primary: PropTypes.bool,
   /**
-   * a callback to handle on click event
+   * A callback to handle on click event
    */
   onClick: PropTypes.func,
+  /**
+   * Custom classnames, Any valid className or a tailwind class (ex: `text-blue-700`).
+   */
+  className: PropTypes.string,
+  /**
+   * Custom style object
+   */
+  style: PropTypes.shape({}),
+  /**
+   * Button Background Color. Any valid color (`red`, `green`. `blue` etc.)
+   */
+  bgColor: PropTypes.string,
+  /**
+   * Button Text Color
+   */
+  color: PropTypes.string,
 };
 
 Button.defaultProps = {
   primary: true,
   onClick: () => null,
   buttonText: 'Button',
+  className: '',
+  style: {},
+  bgColor: 'transparent',
+  color: 'black'
 };
 
 

643771dcbd1b30385af28987d5c3ee06f1fe3541
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-04-28 17:51:17 +0530

    test(button): add new tests

--- src/uielements/button/__tests__/button.spec.js
+++ src/uielements/button/__tests__/button.spec.js
@@ -2,8 +2,37 @@ import React from 'react';
 import { render } from 'test-utils';
 import  Button from 'uielements/button/button.component.js';
 
+// Testing Normal Button
 it('should take a snapshot', () => {
     const { asFragment } = render(<Button buttonText="test button" />)
     
     expect(asFragment(<Button buttonText="test button" />)).toMatchSnapshot()
+});
+
+// Testing Rounded Prop
+it('should take a snapshot', () => {
+    const {asFragment} = render(<Button rounded/>)
+
+    expect(asFragment(<Button rounded/>)).toMatchSnapshot()
+});
+
+// Testing Background Color Prop
+it('should take a snapshot', () => {
+    const {asFragment} = render(<Button bgColor="red"/>)
+
+    expect(asFragment(<Button bgColor="red"/>)).toMatchSnapshot()
+});
+
+// Testing color Prop of the Button 
+it('should take a snapshot', () => {
+    const {asFragment} = render(<Button color="red" />)
+
+    expect(asFragment(<Button color="red"/>)).toMatchSnapshot()
+});
+
+// Testing classname of Button
+it('should take a snapshot', () => {
+    const {asFragment} = render(<Button className="shadow" />)
+
+    expect(asFragment(<Button className="shadow" />)).toMatchSnapshot()
 });

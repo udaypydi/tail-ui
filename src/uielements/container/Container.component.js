0bd539a612d5c6d86ab9ba01fad1c1f2f6dbe9bf
udaypydi <udaypydi333@gmail.com>
2020-04-28 20:12:43 +0530

    feat(container): eslint fixes

--- src/uielements/container/Container.component.js
+++ src/uielements/container/Container.component.js
@@ -3,65 +3,70 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 function Container(props) {
-    const { 
-        className, 
-        raised, 
-        fluid,
-        rounded,
-        children,
-        vertical,
-    } = props;
+  const {
+    className,
+    raised,
+    fluid,
+    rounded,
+    children,
+    vertical,
+  } = props;
 
-    return (
-        <div className={
+  return (
+    <div className={
             classnames(
-                "flex",
-                {
-                    "shadow-md": raised,
-                    "w-full": fluid,
-                    "rounded": rounded,
-                    "flex-col": vertical,
-                    "flex-row": !vertical,
-                },
-                className,
+              'flex',
+              {
+                'shadow-md': raised,
+                'w-full': fluid,
+                rounded,
+                'flex-col': vertical,
+                'flex-row': !vertical,
+              },
+              className,
             )
-        }>
-            {children}
-        </div>
-    )
+        }
+    >
+      {children}
+    </div>
+  );
 }
 
 Container.propTypes = {
-     /**
+  /**
      * Custom classes to container. A valid css class name or a tailwind class name.
      */
-    className: PropTypes.string,
-    /**
-     * Adds a shadow to the container. The shadow can be changed by passing tailwind classNames (`shadow`, `shadow-md`, `shadow-lg`)
+  className: PropTypes.string,
+  /**
+     * Adds a shadow to the container.
+     * The shadow can be changed by passing tailwind classNames (`shadow`, `shadow-md`, `shadow-lg`)
      */
-    raised: PropTypes.bool,
-    /**
+  raised: PropTypes.bool,
+  /**
      * A fluid container takes full width of the parent.
      */
-    fluid: PropTypes.bool,
-    /**
-     * Adds a default `border-radius` to the container. The radius can be changed by passing tailwind classNames (`radius`, `radius-md`, `radius-lg`)
+  fluid: PropTypes.bool,
+  /**
+     * Adds a default `border-radius` to the container.
+     * The radius can be changed by passing tailwind classNames
+     * (`radius`, `radius-md`, `radius-lg`)
      */
-    rounded: PropTypes.bool,
-    children: PropTypes.node,
-    /**
-     * Adds vertical flex to the container. Defaults to false, renders the children horizontally by default.
+  rounded: PropTypes.bool,
+  children: PropTypes.node,
+  /**
+     * Adds vertical flex to the container. Defaults to false,
+     * renders the children horizontally by default.
      */
-    vertical: PropTypes.bool,
-}
+  vertical: PropTypes.bool,
+};
 
 Container.defaultProps = {
-    className: '',
-    raised: false,
-    fluid: false,
-    rounded: false,
-    children: null,
-    vertical: false,
+  className: '',
+  raised: false,
+  fluid: false,
+  rounded: false,
+  children: null,
+  vertical: false,
 };
 
 export default Container;

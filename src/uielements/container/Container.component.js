e444b93564c37abc9222841e0d58f534e9d4db95
udaypydi <udaypydi333@gmail.com>
2020-04-28 19:53:28 +0530

    feat(container): Add explanation in documentation

--- src/uielements/container/Container.component.js
+++ src/uielements/container/Container.component.js
@@ -26,9 +26,21 @@ function Container(props) {
 }
 
 Container.propTypes = {
+     /**
+     * Custom classes to container. A valid css class name or a tailwind class name.
+     */
     className: PropTypes.string,
+    /**
+     * Adds a shadow to the container. The shadow can be changed by passing tailwind classNames (`shadow`, `shadow-md`, `shadow-lg`)
+     */
     raised: PropTypes.bool,
+    /**
+     * A fluid container takes full width of the parent.
+     */
     fluid: PropTypes.bool,
+    /**
+     * Adds a default `border-radius` to the container. The radius can be changed by passing tailwind classNames (`radius`, `radius-md`, `radius-lg`)
+     */
     rounded: PropTypes.bool,
 }
 

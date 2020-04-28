901f5ab321d982a55d8b8b9e3231d2e75f459cb7
udaypydi <udaypydi333@gmail.com>
2020-04-28 19:50:14 +0530

    feat(container): rounded container added

--- src/uielements/container/Container.component.js
+++ src/uielements/container/Container.component.js
@@ -3,7 +3,12 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 function Container(props) {
-    const { className, raised, fluid } = props;
+    const { 
+        className, 
+        raised, 
+        fluid,
+        rounded,
+    } = props;
 
     return (
         <div className={
@@ -11,6 +16,7 @@ function Container(props) {
             {
                 "shadow-md": raised,
                 "w-full": fluid,
+                "rounded": rounded,
             },
             className,
             )
@@ -23,12 +29,14 @@ Container.propTypes = {
     className: PropTypes.string,
     raised: PropTypes.bool,
     fluid: PropTypes.bool,
+    rounded: PropTypes.bool,
 }
 
 Container.defaultProps = {
     className: '',
     raised: false,
     fluid: false,
+    rounded: false,
 };
 
 export default Container;
--- src/uielements/container/Readme.md
+++ src/uielements/container/Readme.md
@@ -14,5 +14,12 @@ Raised Container:
 Fluid Container:
 
 ```js
-<Container className="w-full h-20 border border-gray-200" fluid />
+<Container className="h-20 border border-gray-200" fluid />
 ```
+
+Rounded Container:
+
+```js
+<Container className="h-20 border border-gray-200" rounded />
+```
+

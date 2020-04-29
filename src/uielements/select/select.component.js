4972043e035759c137f71308cd0806daef33ffd2
udaypydi <udaypydi333@gmail.com>
2020-04-29 23:52:04 +0530

    fix(select): eslint fixes added

--- src/uielements/select/select.component.js
+++ src/uielements/select/select.component.js
@@ -3,48 +3,52 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 function Select(props) {
-    const { options, containerClass, optionClass } = props;
-    return (
-        <select 
-            id="tailui-select" 
-            className={classnames(
-                "pt-2",
-                containerClass,
-            )}
-        >
-            {
-                options.map(op => (
-                    <option 
-                        value={op.value} 
-                        className={classnames("pt-5", optionClass)}
-                    >{op.name}</option>
+  const { options, containerClass, optionClass } = props;
+  return (
+    <select
+      id="tailui-select"
+      className={classnames(
+        'pt-2',
+        containerClass,
+      )}
+    >
+      {
+                options.map((op) => (
+                  <option
+                    value={op.value}
+                    className={classnames('pt-5', optionClass)}
+                  >
+                    {op.name}
+                  </option>
                 ))
             }
-        </select>
-    );
+    </select>
+  );
 }
 
 Select.propTypes = {
-    /**
+  /**
      * Options to render in select.
      */
-    options: PropTypes.array,
+  options: PropTypes.shape([]),
 
-    /**
-     * Custom classnames to select container, Any valid className or a tailwind class (ex: `text-blue-700`).
+  /**
+     * Custom classnames to select container,
+     * Any valid className or a tailwind class (ex: `text-blue-700`).
      */
-    containerClass: PropTypes.string,
+  containerClass: PropTypes.string,
 
-    /**
-     * Custom classnames to option, Any valid className or a tailwind class (ex: `text-blue-700`).
+  /**
+     * Custom classnames to option,
+     * Any valid className or a tailwind class (ex: `text-blue-700`).
      */
-    optionClass: PropTypes.string,
+  optionClass: PropTypes.string,
 };
 
 Select.defaultProps = {
-    options: [],
-    className: '',
-    optionClass: '',
+  options: [],
+  containerClass: '',
+  optionClass: '',
 };
 
 export default Select;

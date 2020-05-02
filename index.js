38f6164e9d2a34b45709c3e96cc371f8f131e73d
udaypydi <udaypydi333@gmail.com>
2020-05-02 22:08:28 +0530

    fix(imports): container imports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Button from 'uielements/button/button.component';
 import Input from 'uielements/input/input.component';
-import Container from 'uielements/container/container.component';
+import Container from 'uielements/container/Container.component';
 import Header from 'uielements/header/header.component';
 import Image from 'uielements/image/image.component';
 import Select from 'uielements/select/select.component';

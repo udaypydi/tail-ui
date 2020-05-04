57d8be69620dcd08c2aa33ca1f8550d62138fe8d
udaypydi <udaypydi333@gmail.com>
2020-05-04 22:56:12 +0530

    tests(container): container test cases added

--- src/uielements/container/__tests__/container.spec.js
+++ src/uielements/container/__tests__/container.spec.js
@@ -7,3 +7,33 @@ it('should take a snapshot', () => {
     
     expect(asFragment(<Container />)).toMatchSnapshot()
 });
+
+it('should render a raised container', () => {
+    const { asFragment } = render(<Container className="w-full h-20 border border-gray-200" raised />)
+    
+    expect(asFragment(<Container className="w-full h-20 border border-gray-200" raised />)).toMatchSnapshot()
+});
+
+it('should render a fluid container', () => {
+    const { asFragment } = render(<Container className="h-20 border border-gray-200" fluid />)
+    
+    expect(asFragment(<Container className="h-20 border border-gray-200" fluid />)).toMatchSnapshot()
+});
+
+it('should render a rounded container', () => {
+    const { asFragment } = render(<Container className="h-20 border border-gray-200" rounded />)
+    
+    expect(asFragment(<Container className="h-20 border border-gray-200" rounded />)).toMatchSnapshot()
+});
+
+it('should render a rounded container', () => {
+    const { asFragment } = render(<Container className="h-20 border border-gray-200 p-2" vertical>
+    <p>Hello!!!</p>
+    <p>Welcome to TailUI</p>
+</Container>)
+    
+    expect(asFragment(<Container className="h-20 border border-gray-200 p-2" vertical>
+    <p>Hello!!!</p>
+    <p>Welcome to TailUI</p>
+</Container>)).toMatchSnapshot()
+});

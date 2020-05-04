25da551b949be35d905968296bd771fb0c8d88b3
udaypydi <udaypydi333@gmail.com>
2020-05-04 22:50:04 +0530

    tests(button): add button test cases

--- src/uielements/button/__tests__/button.spec.js
+++ src/uielements/button/__tests__/button.spec.js
@@ -13,3 +13,22 @@ it ('should render a button with text color', () => {
     
     expect(asFragment(<Button color="green"  />)).toMatchSnapshot()
 });
+
+it ('should render a button with custom styles', () => {
+    const { asFragment } = render(<Button  style={{ background: '#0077b5', color: '#ffffff' }}  />)
+    
+    expect(asFragment(<Button  style={{ background: '#0077b5', color: '#ffffff' }}  />)).toMatchSnapshot()
+});
+
+it ('should render a button with custom tailwind classes', () => {
+    const { asFragment } = render(<Button className="text-green-500 border-green-500" />)
+    
+    expect(asFragment(<Button className="text-green-500 border-green-500" />)).toMatchSnapshot()
+});
+
+it ('should render a rounded button', () => {
+    const { asFragment } = render(<Button rounded />)
+    
+    expect(asFragment(<Button rounded />)).toMatchSnapshot()
+});
+

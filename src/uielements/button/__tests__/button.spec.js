210d796d5d887a9411b971c979c80893727de05d
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-05-05 17:53:37 +0530

    fix(button test): Resolved merge conflict by incorporating both suggestions.

--- src/uielements/button/__tests__/button.spec.js
+++ src/uielements/button/__tests__/button.spec.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import { render } from 'test-utils';
-import  Button from 'uielements/button/button.component.js';
+import Button from 'uielements/button/button.component.js';
 
 // Testing Normal Button
 it('should take a snapshot', () => {
     const { asFragment } = render(<Button buttonText="test button" />)
-    
+
     expect(asFragment(<Button buttonText="test button" />)).toMatchSnapshot()
 });
 
 // Testing Rounded Prop
 it('should take a snapshot', () => {
-    const {asFragment} = render(<Button rounded/>)
+    const { asFragment } = render(<Button rounded />)
 
-    expect(asFragment(<Button rounded/>)).toMatchSnapshot()
+    expect(asFragment(<Button rounded />)).toMatchSnapshot()
 });
 
 // Testing Background Color Prop
 it('should take a snapshot', () => {
-    const {asFragment} = render(<Button bgColor="red"/>)
+    const { asFragment } = render(<Button bgColor="red" />)
 
-    expect(asFragment(<Button bgColor="red"/>)).toMatchSnapshot()
+    expect(asFragment(<Button bgColor="red" />)).toMatchSnapshot()
 });
 
 // Testing color Prop of the Button 
 it('should take a snapshot', () => {
-    const {asFragment} = render(<Button color="red" />)
+    const { asFragment } = render(<Button color="red" />)
 
-    expect(asFragment(<Button color="red"/>)).toMatchSnapshot()
+    expect(asFragment(<Button color="red" />)).toMatchSnapshot()
 });
 
 // Testing classname of Button
 it('should take a snapshot', () => {
-    const {asFragment} = render(<Button className="shadow" />)
+    const { asFragment } = render(<Button className="shadow" />)
 
     expect(asFragment(<Button className="shadow" />)).toMatchSnapshot()
 });

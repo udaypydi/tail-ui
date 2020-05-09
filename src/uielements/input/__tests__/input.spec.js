6f8d3429c0dc7d3f6b85096ff64682f75667e851
Venkata Harish Kumar Mamidi <harish.mamidi.kumar@gmail.com>
2020-05-09 19:24:29 +0530

    test(input): input test case added

--- src/uielements/input/__tests__/input.spec.js
+++ src/uielements/input/__tests__/input.spec.js
@@ -1,9 +1,57 @@
 import React from 'react';
 import { render } from 'test-utils';
-import  Input from 'uielements/input/input.component.js';
+import Input from 'uielements/input/input.component.js';
 
 it('should take a snapshot', () => {
     const { asFragment } = render(<Input />);
-    
+
     expect(asFragment(<Input />)).toMatchSnapshot();
+});
+
+it('should render a rounded input', () => {
+    const { asFragment } = render(<Input rounded />);
+
+    expect(asFragment(<Input rounded />)).toMatchSnapshot();
+});
+
+it('should render a disabled input', () => {
+    const { asFragment } = render(<Input disabled />);
+
+    expect(asFragment(<Input disabled />)).toMatchSnapshot();
+});
+
+it('should render a fluid input', () => {
+    const { asFragment } = render(<Input fluid />);
+
+    expect(asFragment(<Input fluid />)).toMatchSnapshot();
+});
+
+it('should render a placeholder input', () => {
+    const { asFragment } = render(<Input placeholder="Hello world!" />);
+
+    expect(asFragment(<Input placeholder="Hello world!" />)).toMatchSnapshot();
+});
+
+it('should render a different type input', () => {
+    const { asFragment } = render(<Input type="checkbox" />);
+
+    expect(asFragment(<Input type="checkbox" />)).toMatchSnapshot();
+});
+
+it('should render a styled input', () => {
+    const { asFragment } = render(<Input style={{ background: '#0077b5', color: '#ffffff' }} />);
+
+    expect(asFragment(<Input style={{ background: '#0077b5', color: '#ffffff' }} />)).toMatchSnapshot();
+});
+
+it('should render new classNames for input', () => {
+    const { asFragment } = render(<Input className="p-1 m-1" />);
+
+    expect(asFragment(<Input className="p-1 m-1" />)).toMatchSnapshot();
+});
+
+it('should render a error input', () => {
+    const { asFragment } = render(<Input error />);
+
+    expect(asFragment(<Input error />)).toMatchSnapshot();
 });

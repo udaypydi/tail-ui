20a8fa29c855b1149f6c323762270a27055c39c5
udaypydi <udaypydi333@gmail.com>
2020-08-16 23:45:45 +0530

    feat(add packages): add build related packages

---
+++ src/uielements/index.js
@@ -0,0 +1,18 @@
+import Button from 'uielements/button/button.component';
+import Input from 'uielements/input/input.component';
+import Container from 'uielements/container/Container.component';
+import Header from 'uielements/header/header.component';
+import Image from 'uielements/image/image.component';
+import Select from 'uielements/select/select.component';
+
+const Components = {
+    Button,
+    Input,
+    Container,
+    Header,
+    Image,
+    Select,
+};
+
+
+export default Components;
